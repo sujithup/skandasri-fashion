20eb9b054b73665ca8a089746251a085eafce3df
Sujithup <sujithup24@gmail.com>
2025-03-02 12:50:15 +0530

    meta

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,20 @@ import Discounts from "./components/Discount/discount";
 import NewArrivals from "./components/NewArrivals/NewArrivals";
 import Footbar from "./components/Footbar/footbar";
 import CopyRight from "./components/Footbar/copyright";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <Hero />
-      <Discounts/>
-      <NewArrivals/>
-      <Footbar/>
-      <CopyRight/>
+      <Discounts />
+      <NewArrivals />
+      <Footbar />
+      <CopyRight />
     </>
   );
 }
