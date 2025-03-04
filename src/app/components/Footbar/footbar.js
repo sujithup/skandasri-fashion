1bf468b13bb55392251a47cfbe2240c7a4bf8c49
Sujithup <sujithup24@gmail.com>
2025-03-05 00:09:58 +0530

    change

--- src/app/components/Footbar/footbar.js
+++ src/app/components/Footbar/footbar.js
@@ -8,7 +8,7 @@ import SocialMedia from "./socialMedia";
 const Footbar = () => {
   return (
     <footer className="flex items-center justify-center border-t-[1px] border-purple p-[24px] md:p-[48px]">
-      <h2 className="sr-only">Footer</h2>
+      <h2 className="sr-only">Footers</h2>
       <div className="flex w-full max-w-full flex-col items-start justify-between md:w-[1440px] md:flex-row">
         <Categories />
         <div className="my-8 flex flex-col items-center justify-center gap-8 md:my-0">
